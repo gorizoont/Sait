68463e8126efd77f86a95f2c8b1bb5d36f66f431
gorizzont <39597252+gorizzont@users.noreply.github.com>
2022-08-11 15:00:55 +0300

    daaaaaa

--- Sait/Program.cs
+++ Sait/Program.cs
@@ -5,7 +5,7 @@ using Sait.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DBmain");
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
